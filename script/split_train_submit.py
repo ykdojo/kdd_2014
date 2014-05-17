7a5f320131ba9c2c6c21dfa5d7b8cb96d19c1b67
Yosuke Sugishita <yosuke.sugi@gmail.com>
2014-05-17 21:52:14 +0900

    split_train_submit.ipynb and .py DONE

--- script/split_train_submit.py
+++ script/split_train_submit.py
@@ -1,24 +1,23 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sat May 17 20:22:16 2014
 
 @author: yosukesugishita
 """
+import os
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 # Split projects.csv into train.csv and submit.csv
-
 import pandas as pd
-import os
 import numpy as np
 
-this_filepath = os.path.realpath(__file__)
-home_dir = os.path.dirname(os.path.dirname(this_filepath))
-
-#filepath = home_dir + "/data/small/projects_h1000.csv" # for testing
-filepath = home_dir + "/data/original/projects.csv"
+filepath = "../data/original/projects.csv"
 dat = pd.read_csv(filepath)
 
 slice_index = np.where(dat.date_posted=='2013-12-31')[0][0]
 submit = dat.iloc[0:slice_index,]
 train = dat.iloc[slice_index:, ]
+
 submit = submit.sort('projectid', ascending=False) # need to sort this for submission later
+
+submit.to_csv("../data/intermediate/submit.csv", index=False)
+train.to_csv("../data/intermediate/train.csv", index=False)
