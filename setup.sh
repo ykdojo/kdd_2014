08ca7335a3eec8a96d747737bc6739c4bc597a6b
Yosuke Sugishita <yosuke.sugi@gmail.com>
2014-05-17 17:19:58 +0900

    Cleaned the setup.sh a bit

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ head -n1000 data/original/projects.csv > data/small/projects_h1000.csv
 head -n1000 data/original/outcomes.csv > data/small/outcomes_h1000.csv
 head -n1000 data/original/resources.csv > data/small/resources_h1000.csv
 head -n1000 data/original/essays.csv > data/small/essays_h1000.csv
-#sed 's/^M//g' data/original/essays_h1000.csv > data/original/essays_h1000.csv # this is needed for reading this file in Excel
+sed -i "" 's///g' data/small/essays_h1000.csv # this is needed for reading this file in Excel
 
 head -n1 data/original/projects.csv > data/small/projects_t1000.csv
 tail -n1000 data/original/projects.csv >> data/small/projects_t1000.csv
